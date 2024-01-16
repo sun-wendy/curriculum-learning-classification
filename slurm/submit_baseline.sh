adba41423d456c22a30dfd8676c8ef9e5d60ccca
Wendy Sun <wendysun@mit.edu>
2024-01-16 07:32:10 -0500

    Updated version

--- slurm/submit_baseline.sh
+++ slurm/submit_baseline.sh
@@ -2,14 +2,19 @@
 #SBATCH --job-name=train_baseline
 #SBATCH -o log/%j-train_baseline.log
 #SBATCH -c 10
-#SBATCH --gres=gpu:volta:1
+#SBATCH --gres=gpu:1
 
-source source /mnt/xfs/home/wendysun/curriculum_learning/stats/bin/activate
+export PATH=/mnt/xfs/home/wendysun/curriculum_learning:$PATH
+
+export OMP_NUM_THREADS=1
+export USE_NNPACK=0
+
+sleep 0.1
 
 for dataset_type in 'foreground' 'composite' 'mix'
 do
-    python curriculum_learning/train_baseline.py \
-    --epochs 10 \
+    python train_baseline.py \
+    --epochs 1 \
     --dataset_type $dataset_type \
     --plot_name baseline_$dataset_type
 done
--- train_setup.py
+++ train_setup.py
@@ -8,7 +8,9 @@ def train(model, trainloader, optimizer, criterion, device):
     train_running_loss = 0.0
     train_running_correct = 0
     counter = 0
+    print(len(trainloader))
     for i, data in tqdm(enumerate(trainloader), total=len(trainloader)):
+        print('Inside for loop...')
         counter += 1
         image, labels = data
         image = image.to(device)
