b543dce884fa447a3270c882504191ceb921a029
Wendy Sun <wendysun@mit.edu>
2024-02-17 11:29:39 -0500

    Script for using 3 GPUs

--- slurm/submit_fore_baseline.sh
+++ slurm/submit_fore_baseline.sh
@@ -2,7 +2,8 @@
 #SBATCH --job-name=train_baseline
 #SBATCH -o log/%j-train_baseline.log
 #SBATCH -c 10
-#SBATCH --gres=gpu:v100:1
+#SBATCH --gres=gpu:v100:3
+#SBATCH --time=48:00:00
 
 export PATH=/mnt/xfs/home/wendysun/curriculum_learning:$PATH
 
@@ -14,13 +15,7 @@ sleep 0.1
 module load conda
 source activate cl_new
 
-LAYERS=(18)
-
-for i in "${LAYERS[@]}"
-do
-    python train_baseline.py \
-    --num_layers $i \
-    --epochs 200 \
-    --dataset_type 'foreground' \
-    --plot_name $i'_baseline_foreground'
-done
+python train_baseline.py \
+--epochs 200 \
+--dataset_type 'foreground' \
+--plot_name $i'_baseline_foreground'
