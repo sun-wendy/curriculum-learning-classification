7b979610789a11587cecbb4d0917b16e52ae3c50
Wendy Sun <wendysun@mit.edu>
2024-02-21 22:00:18 -0500

    Changed type of GPU

--- slurm/submit_fore_baseline.sh
+++ slurm/submit_fore_baseline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=train_baseline
 #SBATCH -o log/%j-train_baseline.log
-#SBATCH -c 28
-#SBATCH --gres=gpu:v100:7
+#SBATCH -c 32
+#SBATCH --gres=gpu:h100:8
 #SBATCH --time=96:00:00
 
 export PATH=/mnt/xfs/home/wendysun/curriculum_learning:$PATH
