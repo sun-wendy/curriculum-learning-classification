63635f103d2a2da3de689d9dbfc5f11bf364a6f3
Wendy Sun <wendysun@mit.edu>
2024-03-03 14:29:39 -0500

    Changed # of GPUs

--- slurm/submit_fore_baseline.sh
+++ slurm/submit_fore_baseline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=train_baseline
 #SBATCH -o log/%j-train_baseline.log
-#SBATCH -c 24
-#SBATCH --gres=gpu:v100:6
+#SBATCH -c 32
+#SBATCH --gres=gpu:v100:8
 #SBATCH --time=200:00:00
 
 export PATH=/mnt/xfs/home/wendysun/curriculum_learning:$PATH
